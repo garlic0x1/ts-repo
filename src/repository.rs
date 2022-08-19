f3d4c3605df7b6206a5b838c471f3cb649749d84
garlic0x1 <garlic1@protonmail.com>
2022-08-19 15:28:02 -0700

    byep

--- src/repository.rs
+++ src/repository.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use super::resolved::*;
-use ts_cursor::file::*;
+use ts_cursor::{cursor::*, file::*, traverser::*};
 
 pub enum Language {
     PHP,
@@ -9,17 +11,45 @@ pub enum Language {
 pub struct Repository<'a> {
     files: Vec<File>,
     language: Language,
-    resolved: Vec<Resolved<'a>>,
+    resolved: HashMap<String, Resolved<'a>>,
 }
 
 impl<'a> Repository<'a> {
-    pub fn from_files(files: Vec<File>, language: Language) -> Self {
-        Self {
-            files,
+    pub fn from_files(files: &'a Vec<File>, language: Language) -> Self {
+        let mut s = Self {
+            files: files.to_owned(),
             language,
-            resolved: vec![],
-        }
+            resolved: HashMap::new(),
+        };
+
+        files
+            .iter()
+            .map(|f| f.cursor(STKind::Abstract))
+            .for_each(|cur| {
+                s.resolve(cur);
+            });
+
+        s
     }
 
-    pub fn resolve(&mut self) {}
+    fn resolve(&mut self, cursor: Cursor<'a>) {
+        Traversal::from_cursor(&cursor, STKind::Abstract)
+            .filter_map(|mot| match mot {
+                Order::Enter(cur) => Some(cur),
+                _ => None,
+            })
+            .for_each(|cur| match cur.kind() {
+                "function_definition" | "method_declaration" => {
+                    if let Some(n) = cur.name(true) {
+                        let res = Resolved::new(cur, ResKind::Function);
+                        self.resolved.insert(n.to_owned(), res);
+                    }
+                }
+                "program" => {
+                    let res = Resolved::new(cur, ResKind::Root);
+                    self.resolved.insert("ROOT".to_owned(), res);
+                }
+                _ => (),
+            });
+    }
 }
